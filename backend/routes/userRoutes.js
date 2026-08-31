92489ec82cdc70243859f07e31034a8235ddf151
red <red095821@gmail.com>
2026-08-31 08:18:01 +0530

    userRoutes cleared and authentication and authorisation logic added, changes in backend completed, changes in frontend are yet to be done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,77 @@
 // backend/routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtokens');
 const User = require('../models/User');
 const Organization = require('../models/Organization');
+
+router.post('/register', async(req, res)=>{
+    try{
+        const {userName, email, password, orgName}=req.body;
+
+        let existingOrg=await Organization.findOne({name: orgName});
+        if(!existingOrg){
+            existingOrg=await Organization.create({name: orgName});
+        }
+
+        const safeOrgId=existingOrg._id.toString();
+
+        const salt=await bcrypt.genSalt(10);
+        const hashedPassword=await bcrypt.hash(password, salt);
+
+        const newUser=await User.create({
+            name:userName,
+            email:email,
+            password:hashedPassword,
+            organization:safeOrgId
+        });
+
+        res.status(201).json({message:'User securely created'});
+    }catch(error){
+        console.error('Registration error: ', error);
+        res.status(500).json({error:'Failed to register'});
+    }
+});
+
+router.post('/login', async(req, res)=>{
+    try{
+        const {email, password}=req.body;
+        const user =await User.findOne({email}).populate('organization');
+        if(!user){
+            return res.status(400).json({error:"Invalid email or password"});
+        }
+        const isMatch=await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({error:"Invalid email or password"});
+        }
+        const token=jwt.sign(
+            {userId:user._id, orgId:user.organization._id},
+            process.env.JWT_SECRET,
+            {expressIn:'id'}
+        );
+        res.status(200).json({
+            message:'Login Succesful',
+            token:token,
+            user:{
+                _id:user._id,
+                name:user.name,
+                email:user.email,
+                organization:user.organization
+            }
+        });
+    }catch(error){
+        console.error("Login error: ", error);
+        res.status(500).json({error: "Failed to login"});
+    }
+});
 
 //Delete a User from database
 router.delete('/:id', async(req,res)=>{
     try{
         //Extracting id from route parameters
         const userId=req.params.id;
-        console.log(userId);
+        //console.log(userId);
         //early fallback if user id dont exist
         if (!userId){
             console.log('Id not found');
@@ -20,47 +82,6 @@ router.delete('/:id', async(req,res)=>{
     }
     catch{
         res.status(500).json({message:"Server database error", error:error.message});
-    }
-});
-
-//Create a new Organization and a User inside it
-router.post('/register',async(req,res)=>{
-    try{
-        const {userName, email, orgName}=req.body;
-        //Checking if organization already exist
-        let existingOrg = await Organization.findOne({name:orgName});
-        //If organization does not exist then create it
-        if(!existingOrg){
-            existingOrg=await Organization.create({name:orgName});
-            console.log(`Created new Organization: ${existingOrg.orgName} with Id ${existingOrg._id}`);
-        }
-        else{
-            console.log(`Joined existing Organization: ${existingOrg.name} with Id ${existingOrg._id}`);
-        }
-        const safeOrgId = existingOrg._id.toString();
-        console.log(`Attempting to create User with safeOrgId: ${safeOrgId}`);
-        //Create the user and link to Organization
-        const newUser=await User.create({
-            name:userName,
-            email:email,
-            organization:safeOrgId
-        });
-        res.status(201).json({message:'user created', user:newUser});
-    }
-    catch(error){
-        console.error(error);
-        res.status(500).json({error:"Something went wrong"});
-    }
-});
-
-//Get all users and their organization details
-router.get('/', async (req, res) => {
-    try {
-        // .populate() tells MongoDB to fetch the actual Organization data, not just the ID!
-        const users = await User.find().populate('organization');
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ error: "Something went wrong" });
     }
 });
 
